2b1156bc210d17f67d61876adcffc2efbe95b8c6
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-09-25 08:03:28 +0000

    Get and post requests

---
+++ src/items/items.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get , Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item-dto';
+//@Body works like req.body
+
+@Controller('items')
+export class ItemsController {
+    @Get()
+    findAll() : string {
+        return 'Get all items';
+    }
+
+    @Post()
+    create(@Body() createItemDto : CreateItemDto) : string {
+        return `Name : ${createItemDto.name} Desc : ${createItemDto.description}`;
+    }
+}
