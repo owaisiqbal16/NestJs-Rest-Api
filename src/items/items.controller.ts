0e61350a144b6b4bdc7a2c827b965fc71a01d29a
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-09-25 16:53:24 +0500

    Done with CRUD using mongodb

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -9,7 +9,7 @@ export class ItemsController {
     constructor( private readonly itemsService: ItemsService) {}
 
     @Get()
-    findAll() : Item[] {
+    async findAll() : Promise<Item[]> {
         return this.itemsService.findAll();
     }
 
@@ -18,23 +18,23 @@ export class ItemsController {
     //     return `Item ${param.id}`
     // }
     @Get(':id')
-    findOne(@Param('id') id) : Item{
+    findOne(@Param('id') id) : Promise<Item>{
         return this.itemsService.findOne(id);
     }
 
     @Post()
-    create(@Body() createItemDto : CreateItemDto) : string {
-        return `Name : ${createItemDto.name} Desc : ${createItemDto.description}`;
+    create(@Body() createItemDto : CreateItemDto) : Promise<Item> {
+        return this.itemsService.create(createItemDto);
     }
 
     @Delete(':id')
-    delete(@Param('id') id) : string {
-        return `Delete ${id}`;
+    delete(@Param('id') id) : Promise<Item> {
+        return this.itemsService.delete(id);
     }
 
     @Put(':id')
-    update(@Body() updateItemDto : CreateItemDto , @Param('id') id) : string {
-        return `Update ${id} - Name ${updateItemDto.name}`;
+    update(@Body() updateItemDto : CreateItemDto , @Param('id') id) : Promise<Item> {
+        return this.itemsService.update(id ,updateItemDto)
     }
 }
 
